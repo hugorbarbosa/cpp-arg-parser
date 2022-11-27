d8b9e075d0900484f116de00c195a43aa1c52917
hugorbarbosa <118065754+hugorbarbosa@users.noreply.github.com>
2022-11-27 16:22:11 +0000

    Arguments parser implementation (#1)
    - Implement a command line parser
    - Add an example of how to use the parser
    - Add CMake files
    - Add unit tests
    - Update README file

--- README.md
+++ README.md
@@ -1,2 +1,180 @@
-# cpp-arg-parser
-A simple C++ command line arguments parser
+# C++ arguments parser
+
+A simple C++ command line arguments parser.
+
+## Table of contents
+
+- [Project structure](#project-structure)
+- [Features](#features)
+- [Requirements](#requirements)
+- [Compilation](#compilation)
+- [Running](#running)
+- [Tests](#tests)
+- [License](#license)
+
+## Project structure
+
+This project is structured in the following directories:
+
+- `cmake`: useful CMake files.
+- `src`: source code of the project.
+- `tests`: files related with tests.
+
+## Features
+
+The command line arguments parser of this project is a simple parser that allows the following:
+
+- Check if a command line option was passed (for example, `-h` or `--help`).
+- Get the value of a command line option (for example, `-f <value>` or `--file <value>`).
+- Show a help message.
+- Show a version information message.
+
+The parser is implemented [here](./src/argParser/). An example of how to use it can be seen in the following code snippet (extracted from [here](./src/main.cpp)).
+
+```C++
+#include <argParser/CmdLineParser.h>
+#include <iostream>
+
+int main(int argc, char const* argv[])
+{
+    // Arguments parser instance
+    const auto argParser{std::make_unique<argParser::CmdLineParser>()};
+
+    // Set application information
+    argParser->setAppName("Application name");
+    argParser->setAppVersion("1.0.0");
+    argParser->setAppDescription("Application description");
+
+    // Set application usage information
+    const std::map<std::string, std::string> options{
+        {"-h, --help", "show help message"},
+        {"-v, --version", "show version"},
+        {"-V, --verbose", "enable verbose messages"},
+        {"-f, --file", "file path"},
+    };
+    argParser->setAppUsageInfo("AppExec", "-f <file_path> [OPTIONS]", options);
+
+    // Parse
+    argParser->parse(argc, argv);
+
+    // Check help option
+    if (argParser->hasOption("-h") || argParser->hasOption("--help")) {
+        argParser->showHelp();
+        return 0;
+    }
+
+    // Check version option
+    if (argParser->hasOption("-v") || argParser->hasOption("--version")) {
+        argParser->showVersion();
+        return 0;
+    }
+
+    // Check verbose option (option example)
+    auto verbose{false};
+    if (argParser->hasOption("-V") || argParser->hasOption("--verbose")) {
+        verbose = true;
+    }
+
+    // File path (required option example, with value)
+    auto option{argParser->getOption("-f")};
+    if (option.empty()) {
+        option = argParser->getOption("--file");
+        if (option.empty()) {
+            std::cout << "Missing file path" << std::endl;
+            std::cout << std::endl;
+            argParser->showHelp();
+        }
+    }
+    if (!option.empty()) {
+        std::cout << "File path: " << option << std::endl;
+    }
+
+    return 0;
+}
+```
+
+The information related to the application (name, version and description) is used when the version information message is shown. The application usage information (executable name and available options) is utilized in the help message.
+
+The following are some results using the options available in this example:
+
+- Specifying the help option:
+    ```sh
+    $ ./SimpleArgParser -h
+    Usage: AppExec -f <file_path> [OPTIONS]
+        -V, --verbose           enable verbose messages
+        -f, --file              file path
+        -h, --help              show help message
+        -v, --version           show version
+    ```
+- Specifying the version option:
+    ```sh
+    $ ./SimpleArgParser -v
+    Application name 1.0.0
+    Application description
+    ```
+- Without specifying options (`-f <file_path>` is required in this example):
+    ```sh
+    $ ./SimpleArgParser
+    Missing file path
+
+    Usage: AppExec -f <file_path> [OPTIONS]
+        -V, --verbose           enable verbose messages
+        -f, --file              file path
+        -h, --help              show help message
+        -v, --version           show version
+    ```
+
+## Requirements
+
+Necessary tools:
+
+- CMake: system to manage the build process.
+- C++ compiler: for software compilation.
+
+## Compilation
+
+The CMake options for configuration of this project are:
+
+| CMake option | Description | Default value |
+| --- | --- | --- |
+| BUILD_TESTS | Build unit tests | OFF |
+
+The following commands can be utilized to configure the project (example for Debug configuration):
+
+```sh
+$ cd <project-directory>
+$ mkdir build-debug
+$ cd build-debug
+$ cmake .. -DCMAKE_BUILD_TYPE=Debug
+```
+
+To compile the software, use the following command:
+
+```sh
+$ cmake --build . -j 4
+```
+
+## Running
+
+After compiling the project, an executable file is created and can be run using the following command (note that the executable may be located in a different directory, depending on the configuration generator):
+
+```sh
+$ ./src/Debug/SimpleArgParser
+```
+
+## Tests
+
+To run the unit tests, use the commands below (note that it is necessary to configure CMake with `BUILD_TESTS` option to ON):
+
+```sh
+$ cd <project-directory>
+$ mkdir build-debug
+$ cd build-debug
+$ cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON
+$ cmake --build . -j 4
+$ ctest
+```
+
+## License
+
+Licensed under the [MIT license](./LICENSE).
---
+++ src/argParser/CmdLineParser.h
@@ -0,0 +1,195 @@
+/**
+ * @file
+ * @copyright Copyright (c) 2022.
+ */
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace argParser {
+
+/**
+ * @brief Command line arguments parser.
+ */
+class CmdLineParser
+{
+public:
+    /**
+     * @brief Constructor.
+     */
+    CmdLineParser() = default;
+
+    /**
+     * @brief Parse the command line arguments.
+     *
+     * @param argc Number of command line arguments.
+     * @param argv Command line arguments.
+     */
+    void parse(const int argc, char const* argv[]);
+
+    /**
+     * @brief Get the command line arguments.
+     *
+     * @return Vector with the command line arguments.
+     */
+    [[nodiscard]] std::vector<std::string> getArgs() const;
+
+    /**
+     * @brief Check if the command line arguments has the option specified.
+     *
+     * This method is useful to check if, for example, a `<option>` is passed to the command line arguments.
+     *
+     * @param option Command line option to check if was passed.
+     *
+     * @return True if the option was passed, false otherwise.
+     */
+    bool hasOption(const std::string& option) const;
+
+    /**
+     * @brief Get the value for the option specified.
+     *
+     * This method is useful to check if, for example, a `<option> <option_value>` is passed to the command line
+     * arguments, and to get the option value.
+     *
+     * @param option Command line option to check if was passed and to get its value.
+     *
+     * @return Command line option value if the option was passed, an empty string otherwise.
+     */
+    std::string getOption(const std::string& option) const;
+
+    /**
+     * @brief Set the application name.
+     *
+     * The application name is used when the version information is shown.
+     *
+     * @param name Application name.
+     */
+    void setAppName(const std::string& name);
+
+    /**
+     * @brief Get the application name.
+     *
+     * @return Application name.
+     */
+    [[nodiscard]] std::string getAppName() const;
+
+    /**
+     * @brief Set the application version.
+     *
+     * The application version is used when the version information is shown.
+     *
+     * @param version Application version.
+     */
+    void setAppVersion(const std::string& version);
+
+    /**
+     * @brief Get the application version.
+     *
+     * @return Application version.
+     */
+    [[nodiscard]] std::string getAppVersion() const;
+
+    /**
+     * @brief Set the application description.
+     *
+     * The application description is used when the version information is shown.
+     *
+     * @param description Application description.
+     */
+    void setAppDescription(const std::string& description);
+
+    /**
+     * @brief Get the application description.
+     *
+     * @return Application description.
+     */
+    [[nodiscard]] std::string getAppDescription() const;
+
+    /**
+     * @brief Show the version information.
+     *
+     * Example:
+     * - Application name: Parser
+     * - Application version: 1.0.0
+     * - Application description: Command line arguments parser
+     * - Output:
+     * @code
+     * Parser 1.0.0
+     * Command line arguments parser
+     * @endcode
+     */
+    void showVersion() const;
+
+    /**
+     * @brief Set the application usage information.
+     *
+     * The application usage information is used when the help message is shown.
+     *
+     * @param exeName Executable name of the application.
+     * @param optionsUsageInfo Usage information of the options.
+     * @param options Map with the options.
+     */
+    void setAppUsageInfo(const std::string& exeName,
+                         const std::string& optionsUsageInfo,
+                         const std::map<std::string, std::string>& options);
+
+    /**
+     * @brief Get the executable name of the application.
+     *
+     * @return Executable name of the application.
+     */
+    [[nodiscard]] std::string getExeAppName() const;
+
+    /**
+     * @brief Get the usage information of the options.
+     *
+     * @return Usage information of the options.
+     */
+    [[nodiscard]] std::string getOptionsUsageInfo() const;
+
+    /**
+     * @brief Get the command line options defined to be used in the help message.
+     *
+     * @return Map with the options.
+     */
+    [[nodiscard]] std::map<std::string, std::string> getOptions() const;
+
+    /**
+     * @brief Show the help message.
+     *
+     * Example:
+     * - exeName: ParserExe
+     * - optionsUsageInfo: [OPTIONS]
+     * - options: "-h, --help", "show help message"
+     * - Output:
+     * @code
+     * Usage: ParserExe [OPTIONS]
+     *
+     *     -h, --help        show help message
+     * @endcode
+     */
+    void showHelp() const;
+
+private:
+    /// Arguments.
+    std::vector<std::string> mArgs{};
+
+    /// Application name.
+    std::string mAppName{};
+    /// Application version.
+    std::string mAppVersion{};
+    /// Application description.
+    std::string mAppDescription{};
+
+    /// Executable name of the application.
+    std::string mExeAppName{};
+    /// Usage information of the options.
+    std::string mOptionsUsageInfo{};
+    /// Map with the options.
+    std::map<std::string, std::string> mOptions{};
+};
+
+} // namespace argParser
